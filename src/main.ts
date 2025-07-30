1461c844ccd0e700ed81bd494b1784340963237d
Mohamed Amarcha <80904831+OzawaKyo@users.noreply.github.com>
2025-07-30 19:31:21 +0200

    new logger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ClassSerializerInterceptor } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { UserService } from './user/user.service';
+import { Logger } from '@nestjs/common';
 
 async function createDefaultAdmin(app: any){
   try{
@@ -23,11 +24,7 @@ async function createDefaultAdmin(app: any){
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = app.get('NestApplicationLogger');
-  app.useLogger(logger);  
-
-
-
+  const logger = new Logger('Bootstrap');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableCors(); 
   // Swagger setup
@@ -44,5 +41,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
+
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
