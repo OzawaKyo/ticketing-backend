a0e46f39372254e336e66d99c9f8bf3f759ecf8a
Mohamed Amarcha <80904831+OzawaKyo@users.noreply.github.com>
2025-07-30 18:49:52 +0200

    admin created at the start

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { UserService } from './user/user.service';
+
+async function createDefaultAdmin(app: any){
+  try{
+    const userService = app.get(UserService);
+    // Create a default admin user
+    await userService.create({
+      prenom: 'Admin',
+      nom: 'ADMIN',
+      email: 'admin2@admin.com',
+      password: 'adminadmin',
+      role: 'admin',
+    });
+  } catch (error) {
+    console.error('Error creating default admin:');
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +34,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  await createDefaultAdmin(app);
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
